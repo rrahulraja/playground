bd5e94605ae551dc02553131193ba18598d157da
Rrahul Raja <rahulraja.ngp@gmail.com>
2021-10-11 19:34:23 +0530

    Added gridtraveller problem

---
+++ dp/gridtraveller/index.js
@@ -0,0 +1,15 @@
+const gridTraveller = (m, n) => {
+    if(m === 1 && n === 1) return 1
+    if(m === 0 || n === 0) return 0
+
+    return gridTraveller(m - 1, n) + gridTraveller(m, n - 1)
+}
+
+console.log(gridTraveller(1, 1))
+console.log(gridTraveller(2, 3))
+console.log(gridTraveller(3, 2))
+console.log(gridTraveller(18, 18))
+
+
+// Time complexity - O(2^(n+m))
+// Space complexity - O(n+m) 
